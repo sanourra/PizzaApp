7bbfd4dbfd1c5b0ce95eb52e2ead3376fca829f3
sanourra <sanourra@gmail.com>
2016-07-07 13:27:50 -0700

    Created About Dialog
    Created an About box with information regarding Pizza App. Also cleaned up menu code so that each menu is its own object.

--- GUIMenu.java
+++ GUIMenu.java
@@ -24,47 +24,70 @@ public class GUIMenu
 	private void makeMenus()
 	{
 		JMenuBar menuBar;
-		JMenu orderMenu;
-		//JMenu helpMenu;
-		JMenuItem menuItem;
 		
 		menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
-        		
-		//set up the order Menu
-		orderMenu = new JMenu("File");
-		orderMenu.setMnemonic(KeyEvent.VK_O); //adds hotkey shortcut to Order menu
-        
-       
-        	//add order menu items below
-        	menuItem = new JMenuItem("New Order"); //tells program that Order has a new item, "New Order"
-        	menuItem.setMnemonic(KeyEvent.VK_N); //tells program that Alt + N will activate New menu item
-        	menuItem.addActionListener(new newListener());//adds action listener and tells it which class to reference
-        	menuItem.setAccelerator(
-				KeyStroke.getKeyStroke(KeyEvent.VK_N,Event.CTRL_MASK)); //sets keyboard shortcut to use CTRL + N to use the New Order option 
-        	orderMenu.add(menuItem); //adds "New Order" to the options in the File menu
-        
-
-        	menuItem = new JMenuItem("Save Order");
-        	menuItem.setMnemonic(KeyEvent.VK_S);
-        	menuItem.addActionListener(new saveListener());
-        	menuItem.setAccelerator(
-        			KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
-        	orderMenu.add(menuItem);
-
-        	orderMenu.addSeparator();
-
-        	menuItem = new JMenuItem("Exit");
-        	menuItem.setMnemonic(KeyEvent.VK_X);
-        	menuItem.addActionListener(new exitListener());
-        	menuItem.setAccelerator(
-        			KeyStroke.getKeyStroke(KeyEvent.VK_Q, Event.CTRL_MASK));
-        	orderMenu.add(menuItem);
-         menuBar.add(orderMenu);//adds file menu to menu bar
+		frame.setJMenuBar(menuBar);
 		
+		//set up the menus
+		menuBar.add(makeOrderMenu());
+		menuBar.add(makeHelpMenu());
+	 }
+        		
+	private JMenu makeOrderMenu()
+	{
+		JMenu menu;
+		JMenuItem menuItem;  
+		//set up the order Menu
+		menu = new JMenu("Order");
+		menu.setMnemonic(KeyEvent.VK_O); //adds hotkey shortcut to Order menu
+
+		//add order menu items below
+		menuItem = new JMenuItem("New Order"); //tells program that Order has a new item, "New Order"
+		menuItem.setMnemonic(KeyEvent.VK_N); //tells program that Alt + N will activate New menu item
+		menuItem.addActionListener(new newListener());//adds action listener and tells it which class to reference
+		menuItem.setAccelerator(
+				KeyStroke.getKeyStroke(KeyEvent.VK_N,Event.CTRL_MASK)); //sets keyboard shortcut to use CTRL + N to use the New Order option 
+		menu.add(menuItem); //adds "New Order" to the options in the File menu
+
+		//add Save Order menu item
+		menuItem = new JMenuItem("Save Order");
+		menuItem.setMnemonic(KeyEvent.VK_S);
+		menuItem.addActionListener(new saveListener());
+		menuItem.setAccelerator(
+				KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
+		menu.add(menuItem);
+
+		menu.addSeparator();
+
+		menuItem = new JMenuItem("Exit");
+		menuItem.setMnemonic(KeyEvent.VK_X);
+		menuItem.addActionListener(new exitListener());
+		menuItem.setAccelerator(
+				KeyStroke.getKeyStroke(KeyEvent.VK_Q, Event.CTRL_MASK));
+		menu.add(menuItem);
+
+		return menu;
+	}
+	
+	private JMenu makeHelpMenu()
+	{
+		JMenu menu;
+		JMenuItem menuItem;  
+		
+		menu = new JMenu("Help"); //set up the Help menu
+		menu.setMnemonic(KeyEvent.VK_H);
+		
+		// add About menu item
+		menuItem = new JMenuItem("About Pizza App");
+		menuItem.setMnemonic(KeyEvent.VK_B);
+		menuItem.addActionListener(new aboutListener());
+		menu.add(menuItem);
+		
+		return menu;
+	}
         
        
-	}
+
 	
 	private class newListener implements ActionListener //program listens for click on New menu option
 	{
@@ -72,7 +95,7 @@ public class GUIMenu
 		{
 			JOptionPane.showMessageDialog(frame, 
 				"The File > New Order menu option was clicked.",
-				"Menu Item Click",
+				"Pizza App",
 				JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
@@ -84,7 +107,7 @@ public class GUIMenu
 		{
 			JOptionPane.showMessageDialog(frame, 
 					"The File > Save Order menu option was clicked",
-					"Menu Item Click",
+					"Pizza App",
 					JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
@@ -96,5 +119,19 @@ public class GUIMenu
 			System.exit(0); //does not return with dialog box, but exits program
 		}
 	}
+	
+	private class aboutListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			JOptionPane.showMessageDialog(frame, 
+					"Pizza App\n\nVersion 1.0\nBuild C20160215-1977\n\n"
+					+ "(c) Copyright Megan Harris 2016\nAll rights reserved\n\n"
+					+ "Intermediate Java Course",
+					"About Pizza App",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
 
+	
 }
